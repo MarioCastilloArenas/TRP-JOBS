0264fb3d34c4d9c06113bf6a591c7bb2de98a1ba
Anthony11A <caluniaadrian32@gmail.com>
2023-04-05 15:10:25 +0200

    index

--- assets/script/indexEmpresa.js
+++ assets/script/indexEmpresa.js
@@ -1,3 +1,5 @@
+// Div contenido
+
 function fMostrarCuenta(){
     let fieldset = document.getElementById('paso1');
     fieldset.style.display = 'flex';
@@ -24,4 +26,48 @@ function fMostrarInformacion(){
     fieldset2.style.display = 'none';
     let fieldset3 = document.getElementById('paso3');
     fieldset3.style.display = 'flex';
+}
+
+// Navegador
+
+function fMostrarMisDatos(){
+    let fieldset = document.getElementById('empresaContenido1');
+    fieldset.style.display = 'flex';
+    let fieldset2 = document.getElementById('empresaContenido2');
+    fieldset2.style.display = 'none';
+    let fieldset3 = document.getElementById('empresaContenido3');
+    fieldset3.style.display = 'none';
+
+}
+function fMostrarMisOfertas(){
+    let fieldset = document.getElementById('empresaContenido1');
+    fieldset.style.display = 'none';
+    let fieldset2 = document.getElementById('empresaContenido2');
+    fieldset2.style.display = 'flex';
+    let fieldset3 = document.getElementById('empresaContenido3');
+    fieldset3.style.display = 'none';
+    
+}
+function fPublicarOferta(){
+    let fieldset = document.getElementById('empresaContenido1');
+    fieldset.style.display = 'none';
+    let fieldset2 = document.getElementById('empresaContenido2');
+    fieldset2.style.display = 'none';
+    let fieldset3 = document.getElementById('empresaContenido3');
+    fieldset3.style.display = 'flex';
+    
+}
+function fCerrarSesion(){
+    
+}
+
+// div class:empresaContenidoOpcionesBtn;
+function fMostrarTodas(){
+
+}
+function fMostrarPendientes(){
+
+}
+function fMostrarFinalizadas(){
+
 }
--- assets/script/indexTransportista.js
+++ assets/script/indexTransportista.js
@@ -29,3 +29,32 @@ function fCerrarSesion(){
     
 }
 
+
+function fMostrarBasicos(){
+    let fieldset = document.getElementById('paso1');
+    fieldset.style.display = 'flex';
+    let fieldset2 = document.getElementById('paso2');
+    fieldset2.style.display = 'none';
+    // let fieldset3 = document.getElementById('paso3');
+    // fieldset3.style.display = 'flex';
+
+}
+function fMostrarProfesionales(){
+    let fieldset = document.getElementById('paso1');
+    fieldset.style.display = 'none';
+    let fieldset2 = document.getElementById('paso2');
+    fieldset2.style.display = 'flex';
+    let fieldset3 = document.getElementById('paso3');
+    fieldset3.style.display = 'none';
+    
+}
+function fMostrarExperiencia(){
+    let fieldset = document.getElementById('paso1');
+    fieldset.style.display = 'none';
+    let fieldset2 = document.getElementById('paso2');
+    fieldset2.style.display = 'none';
+    let fieldset3 = document.getElementById('paso3');
+    fieldset3.style.display = 'flex';
+    
+}
+
